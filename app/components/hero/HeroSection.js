54ef477c6e2012cd0a3cf50ff2cba684e90fd25f
een <121685444+kyeenno@users.noreply.github.com>
2025-04-01 11:11:47 +0300

    Changed Title

--- app/components/hero/HeroSection.js
+++ app/components/hero/HeroSection.js
@@ -1,11 +1,9 @@
-import EmailForm from "./EmailForm";
-
 const HeroSection = () => {
     return (
     <div className="h-screen flex flex-col justify-center items-center bg-[#eeffff]">
         <div className="h-1/2 w-screen bg-[#063838] text-white flex flex-col items-center justify-center px-6 text-center">
             <h1 className="text-8xl md:text-5xl font-bold">
-                Too Many Dashboards? Too Little Clarity? <br></br>You Startup's Key Metrics—In Your Pocket📱
+                {`$Too Many Dashboards? Too Little Clarity? <br></br>You Startup's Key Metrics—In Your Pocket📱`}
             </h1>
             <p className="mt-8 max-w-2xl text-lg">
                 Connect 10+ data sources, manage multiple SaaS projects, make data-driven decisions and get notified when things change.
@@ -16,7 +14,6 @@ const HeroSection = () => {
             Struggling to keep up with your SaaS analytics?
             </h2>
             <p className="mt-6 opacity-75 text-lg font-semibold">Sign Up for Early Access →</p>
-            <EmailForm />
         </div>
     </div>
     );
